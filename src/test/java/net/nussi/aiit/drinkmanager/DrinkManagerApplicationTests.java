e60a5ec7ff666757912e43db501621d7eac99192
DaNussi <daniel.nussbaum@htlstp.at>
2024-02-28 14:44:37 +0100

    Init

---
+++ src/test/java/net/nussi/aiit/drinkmanager/DrinkManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nussi.aiit.drinkmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
